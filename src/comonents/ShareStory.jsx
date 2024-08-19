74a7b4132db14c0ea62a0c6186d62c0b0e1053ff
gunavardhan27 <bgunavardhan27@gmail.com>
2024-08-19 20:00:56 +0530

    fixed the share-story-page

--- src/comonents/ShareStory.jsx
+++ src/comonents/ShareStory.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import TextArea from "./TextArea"
 export default function ShareStory(){
+    
     const [hint,setHint] = useState(true)
     function handleHints(){
         setHint(!hint)
@@ -16,9 +17,10 @@ export default function ShareStory(){
                     onClick={handleHints}>{hint ? 'Disable Hints' : 'Enable Hints'}</button>
                 </div>
 
-                {hint ? <form className="flex flex-col items-start w-full gap-4 basis-11/12">
                 
-                <TextArea first={'Your starting point'} second={'Think back to when your mental health challenge began.What was happening in your life?'} 
+                <form className="flex flex-col items-start w-full gap-4 basis-11/12">
+                
+                {hint ? <div className="w-full"><TextArea first={'Your starting point'} second={'Think back to when your mental health challenge began.What was happening in your life?'} 
                  third={'I first noticed I was struggling when..'}/>
                  <TextArea first={'The challenges you faced'} second={'What were the biggest obstacles you encountered?'} 
                  third={'The hardest part for me was..'}/>
@@ -31,21 +33,20 @@ export default function ShareStory(){
 
                 <TextArea first={'Your message of hope'} second={'What would you say to someone facing similar changes'} 
                 third={'To anyone struggling I want to know...'}/>
-            </form> :
-            <form className="flex flex-col items-start w-full gap-4 basis-11/12 h-24">
-                <div className='grid gird-cols-2 w-full'>
-                <label htmlFor="data" className="mb-2">             
-                </label>
-            <textarea className=" bg-[#E0EDF7] h-20 p-2 text-[#858484]">
-                Start Here...
-            </textarea>
-        </div>
-
-
+                </div> : <TextArea third={'Start here...'} />}
             </form>
-            }
+            
+           
             </div>
         </div>
     </div>
     )
 }
+ // <form className="flex flex-col items-start w-full gap-4 basis-11/12 h-24">
+                {/*<div className='grid gird-cols-2 w-full'>
+                <label htmlFor="data" className="mb-2">             
+                </label>
+            <textarea className=" bg-[#E0EDF7] h-20 p-2 text-[#858484]">
+                Start Here...
+            </textarea>
+        </div>*/}
--- src/comonents/TextArea.jsx
+++ src/comonents/TextArea.jsx
@@ -2,10 +2,10 @@ export default function TextArea({first,second,third}){
     console.log(third)
     return (
         <div className='grid gird-cols-2 w-full'>
-            <label htmlFor="data" className="mb-2">
+            
             <p className="text-xl mb-2">{first}</p>
             <p className="text-md text-[#858484] mb-2">{second}</p>                    
-            </label>
+        
             <textarea className=" bg-[#E0EDF7] h-20 p-2 text-[#858484]">
                 {third}
             </textarea>
