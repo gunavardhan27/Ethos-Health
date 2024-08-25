634a23cb395cab725c8339b4f2723a35f80f77d8
masasandeep <masasandeep12@gmail.com>
2024-08-26 02:00:39 +0530

    about us page and routes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import Header from './comonents/Header';
 import Donations from './comonents/Donations';
 import Support from './comonents/Assistance';
 import ShareStory from './comonents/ShareStory';
+import Aboutus from './comonents/pages/Aboutus';
+import { BrowserRouter, Routes } from 'react-router-dom';
 function App() {
   return (
     <div>
       <Header />
-      <Donations />
-      <Support />
-      <ShareStory />
+      <Routes>
+        <Route path='/support' element = {<Support />}/>
+        <Route path='/about' element = {<Aboutus/>}/>
+        <Route path='/story' element = {<ShareStory/>}/>
+        <Route path='/donations' element = {<Donations/>}/>
+      </Routes>
     </div>
   );
 }
